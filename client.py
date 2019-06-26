50920f55f0c46452e3dbe5fa64ed3353301fce52
Pedro Francescon Cittolin <pfcittolin@gmail.com>
2019-06-26 09:43:27 -0300

    B8ZS implementado (encode e decode)

--- client.py
+++ client.py
@@ -2,22 +2,28 @@ import os
 from socket import *
 from encryption import Crypto
 from bitascii import String
+from encoding import B8ZS
 
-host = "10.183.15.253"
+host = "10.183.7.141"
 port = 13000
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 
 crypto = Crypto('senha')
 
+# test = [1,0,-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,1,-1]
+# print(test)
+# encoded = B8ZS.encode(test)
+# print(encoded)
+# decoded = B8ZS.decode(encoded)
+# print(decoded)
+
 while True:
     data = input("Enter message to send or type 'exit': ")
     encrypted = crypto.encryptString(data)
     bits = String.tobits(encrypted)
-
-    # executar algorítmo b8zs em 'bits'
-
-    UDPSock.sendto(bytes([(i+1) for i in bits]), addr)
+    encoded = B8ZS.encode(bits)
+    UDPSock.sendto(bytes([(i+1) for i in encoded]), addr)
     if data == "exit":
         print("Aplicação encerrada!")
         break
--- encoding.py
+++ encoding.py
@@ -2,11 +2,54 @@ import base64
 
 class B8ZS():
     
-    def encode(self, string):
-    	lastOne = '0'
+    def encode(bits):
     	zeroCount = 0
+    	lastOne = -1
 
-    	for s in string:
+    	for i in range(len(bits)):
     		
+    		if (bits[i] == 0):
+    			zeroCount += 1
+    		else:
+    			zeroCount = 0
+    			lastOne = bits[i]
 
-    def decode(self. string):
+    		if (zeroCount == 8):
+    			zeroCount = 0
+
+    			bits[i] = lastOne
+    			bits[i-1] = -lastOne
+    			bits[i-2] = 0
+    			bits[i-3] = -lastOne
+    			bits[i-4] = lastOne
+    			bits[i-5] = 0
+    			bits[i-6] = 0
+    			bits[i-7] = 0
+
+    	return bits
+    			
+
+    def decode(bits):
+    	lastOne = 0
+
+    	for i in range(len(bits)):
+    		
+    		if (bits[i] != 0):
+    			
+    			if (bits[i] == lastOne):
+    				
+    				lastOne = bits[i]
+    				
+    				bits[i-3] = 0
+    				bits[i-2] = 0
+    				bits[i-1] = 0
+    				bits[i] = 0
+    				bits[i+1] = 0
+    				bits[i+2] = 0
+    				bits[i+3] = 0
+    				bits[i+4] = 0
+
+    			else:
+    				lastOne = bits[i]
+
+    	return bits
